489e8db3e0e069f54d761fbf6a4680711fbe53f8
TheBloxdy123 <lee.edward206@hotmail.com>
2026-06-09 11:20:26 -0700

    Data Processed

--- Final Project/app.py
+++ Final Project/app.py
@@ -1,2 +1,4 @@
-print("hello world!")
-print("hello world!2")
+#Use wc_all_matches to train the model and maybe use wc_top_scorers as credibility or somthing. 
+#You could probably use the wc_all_editions to test the model or trainning. 
+#You could use the wc_2026_matches to predict the winners of the 2026 world cup.
+#its in strings so be careful 
